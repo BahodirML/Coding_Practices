30dfbb5fe91b6f08c05921a23e7d209d9142e466
Bahodir011 <116503420+Bahodir011@users.noreply.github.com>
2023-01-24 17:37:49 +0900

    yangi file lar qo'shildi
    yangi darslar

---
+++ untitled1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 22 01:11:59 2023
+
+@author: bahodiralayorov
+"""
+#print ("Hello world!")
+#print(2+4)
+
+#print("Hello world!")
+
+#print( ' "Tico", "Nexia", \'Damas\'  ko\'rganlar qilar havas')
+
+#salom = "Hello world!"
+#print(salom)
+
+#habar = "BUgun O'zbekistonda bayram ekan"
+#print(habar)
+
+#habar = "Bugun Koreyada bayram ekan"
+#print(habar)
+
+#class = 'yaxshi'
+#print(class)
+
+#radius = 5
+#pi = 3.1415
+#aylana_yuzi = pi*radius**2
+#rint("Radiusi" ,radius, "ga teng aylana yuzi=", aylana_yuzi )
+
+kocha ="Bog'bon"
+mahalla = " Sog'bon"
+tuman = "Bodomzor"
+viloyat = "Samarqand"
+
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+
+
